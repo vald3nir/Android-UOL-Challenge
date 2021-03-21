88244f88b3a9d9cd60402fd0f46f8d39405ad161
Valdenir Severino <vald3nir@gmail.com>
2021-03-21 15:58:56 -0300

    add ImageView Extensions

---
+++ app/src/main/java/com/vald3nir/my_events/core/extensions/ImageViewExtensions.kt
@@ -0,0 +1,19 @@
+package com.vald3nir.my_events.core.extensions
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.squareup.picasso.Picasso
+import com.vald3nir.my_events.R
+
+@BindingAdapter(value = ["loadImage"])
+fun ImageView.bindingLoadImage(url: String?) = loadImage(url)
+
+
+fun ImageView.loadImage(url: String?) {
+    if (!url.isNullOrBlank()) {
+        Picasso.get().load(url).placeholder(R.drawable.ic_launcher_background).into(this)
+    } else {
+        this.setImageResource(R.drawable.ic_launcher_background)
+    }
+}
+
