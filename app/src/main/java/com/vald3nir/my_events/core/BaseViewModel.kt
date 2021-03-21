e4fcaca580987b7a482d4a29d1bfc213d5912a7e
Valdenir Severino <vald3nir@gmail.com>
2021-03-21 15:58:02 -0300

    add base components

---
+++ app/src/main/java/com/vald3nir/my_events/core/BaseViewModel.kt
@@ -0,0 +1,27 @@
+package com.vald3nir.my_events.core
+
+import androidx.lifecycle.ViewModel
+import com.vald3nir.data.DataController
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+
+open class BaseViewModel : ViewModel() {
+
+    private val viewModelJob = SupervisorJob()
+    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+    val dataController = DataController(MainApplication.applicationContext())
+
+
+    fun launch(block: suspend () -> Unit) {
+        uiScope.launch {
+            block()
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+}
---
+++ app/src/main/java/com/vald3nir/my_events/core/MainApplication.kt
@@ -0,0 +1,21 @@
+package com.vald3nir.my_events.core
+
+import android.app.Application
+import android.content.Context
+
+class MainApplication : Application() {
+
+    init {
+        instance = this
+    }
+
+    companion object {
+
+        private var instance: MainApplication? = null
+
+        fun applicationContext(): Context {
+            return instance!!.applicationContext
+        }
+    }
+
+}
